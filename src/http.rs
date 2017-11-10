50678952150cdeff26ee886f8ebe648017e5e6d9
sorz <orz@sorz.org>
2017-11-10 23:54:52 +0800

    Add async handshake on HTTP proxy

--- src/http.rs
+++ src/http.rs
@@ -4,10 +4,11 @@ extern crate futures;
 use std::fmt;
 use std::time::Duration;
 use std::net::{TcpStream, SocketAddr};
-use std::io::{self, Write, BufReader, BufRead, ErrorKind};
+use std::io::{self, Write, Read, BufReader, BufRead, ErrorKind};
 use self::futures::{future, Future};
-use self::tokio_core::reactor::{Handle, Timeout};
+use self::tokio_core::reactor::Handle;
 use self::tokio_core::net as tnet;
+use self::tokio_io::io::{write_all, read_until};
 use ::proxy::ProxyServer;
 
 
@@ -41,22 +42,11 @@ impl ProxyServer for HttpProxyServer {
     fn connect(&self, addr: SocketAddr) -> io::Result<TcpStream> {
         let mut stream = TcpStream::connect(self.addr)?;
         stream.set_nodelay(true)?;
-        // FIXME: would block accepting connections
         stream.set_read_timeout(Some(Duration::from_secs(1)))?;
         stream.set_write_timeout(Some(Duration::from_millis(100)))?;
         debug!("creating proxy tunnel to {} via {}", addr, self.tag());
 
-        let host = match addr {
-            SocketAddr::V4(s) => format!("{}:{}", s.ip(), s.port()),
-            SocketAddr::V6(s) => format!("[{}]:{}", s.ip(), s.port()),
-        };
-        let request = format!(
-            "CONNECT {host} HTTP/1.1\r\n\
-            Host: {host}\r\n\
-            Connection: close\r\n\r\n",
-            host=host
-        );
-        stream.write_all(request.as_bytes())?;
+        stream.write_all(build_request(&addr).as_bytes())?;
 
         let mut reader = BufReader::new(stream);
         let mut line = String::new();
@@ -73,7 +63,7 @@ impl ProxyServer for HttpProxyServer {
                 break;
             }
         }
-        // TODO: may lost data in buffer
+        // FIXME: may lost data in buffer
         let stream = reader.into_inner();
 
         debug!("proxy tunnel connected");
@@ -84,7 +74,56 @@ impl ProxyServer for HttpProxyServer {
 
     fn connect_async(&self, addr: SocketAddr, handle: Handle)
             -> Box<Future<Item=tnet::TcpStream, Error=io::Error>> {
-        unimplemented!()
+        let conn = tnet::TcpStream::connect(&self.addr, &handle);
+        let request = conn.and_then(move |stream| {
+            write_all(stream, build_request(&addr))
+        });
+        let response = request.and_then(|(stream, _)| {
+            let reader = BufReader::new(stream).take(512);
+            read_until(reader, 0x0a, Vec::with_capacity(32))
+        }).and_then(|(reader, status)| {
+            let status = match String::from_utf8(status) {
+                Ok(s) => s,
+                Err(e) => return future::err(io::Error::new(ErrorKind::Other,
+                        format!("fail to parse http response: {}", e))),
+            };
+            if status.starts_with("HTTP/1.1 2") {
+                future::ok(reader.into_inner())
+            } else {
+                let err = format!("proxy return error: {}", status.trim());
+                future::err(io::Error::new(ErrorKind::Other, err))
+            }
+        });
+        let skip = response.and_then(|reader| {
+            let buf = Vec::with_capacity(64);
+            future::loop_fn((reader, buf), |(reader, mut buf)| {
+                buf.clear();
+                let reader = reader.take(2048);
+                read_until(reader, 0x0a, buf).and_then(|(reader, buf)| {
+                    let reader = reader.into_inner();
+                    if buf.len() <= 2 {
+                        Ok(future::Loop::Break(reader))
+                    } else {
+                        Ok(future::Loop::Continue((reader, buf)))
+                    }
+                })
+            })
+        });
+        // FIXME: may lost data in buffer
+        Box::new(skip.map(|reader| reader.into_inner()))
     }
 }
 
+fn build_request(addr: &SocketAddr) -> String {
+        let host = match *addr {
+            SocketAddr::V4(s) => format!("{}:{}", s.ip(), s.port()),
+            SocketAddr::V6(s) => format!("[{}]:{}", s.ip(), s.port()),
+        };
+        let request = format!(
+            "CONNECT {host} HTTP/1.1\r\n\
+            Host: {host}\r\n\
+            Connection: close\r\n\r\n",
+            host=host
+        );
+        return request;
+}

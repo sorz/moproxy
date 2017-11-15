9126b250c72fca0e8014b704e31f42e789654b72
sorz <orz@sorz.org>
2017-11-15 17:30:02 +0800

    Log HTTP headers

--- src/http.rs
+++ src/http.rs
@@ -2,6 +2,7 @@ extern crate tokio_core;
 extern crate tokio_io;
 extern crate futures;
 use std::fmt;
+use std::str;
 use std::net::SocketAddr;
 use std::io::{self, Read, BufReader, ErrorKind};
 use self::futures::{future, Future};
@@ -41,6 +42,7 @@ impl ProxyServer for HttpProxyServer {
     fn connect(&self, addr: SocketAddr, handle: &Handle) -> Box<Connect> {
         let conn = tnet::TcpStream::connect(&self.addr, handle);
         let request = conn.and_then(move |stream| {
+            debug!("connected with {:?}", stream.peer_addr());
             if let Err(e) = stream.set_nodelay(true) {
                 warn!("fail to set nodelay: {}", e);
             };
@@ -50,11 +52,12 @@ impl ProxyServer for HttpProxyServer {
             let reader = BufReader::new(stream).take(512);
             read_until(reader, 0x0a, Vec::with_capacity(32))
         }).and_then(|(reader, status)| {
-            let status = match String::from_utf8(status) {
+            let status = match str::from_utf8(&status) {
                 Ok(s) => s,
                 Err(e) => return future::err(io::Error::new(ErrorKind::Other,
                         format!("fail to parse http response: {}", e))),
             };
+            debug!("recv response: {}", status.trim());
             if status.starts_with("HTTP/1.1 2") {
                 future::ok(reader.into_inner())
             } else {
@@ -62,22 +65,31 @@ impl ProxyServer for HttpProxyServer {
                 future::err(io::Error::new(ErrorKind::Other, err))
             }
         });
+        let skip_headers = |(reader, mut buf): (io::Take<_>, Vec<u8>)| {
+            buf.clear();
+            read_until(reader, 0x0a, buf).and_then(|(reader, buf)| {
+                match str::from_utf8(&buf) {
+                    Err(e) => warn!("cannot parse http header: {}", e),
+                    Ok(header) => {
+                        let header = header.trim();
+                        if header.is_empty() {
+                            debug!("all headers passed, pipe established");
+                            return Ok(future::Loop::Break(reader));
+                        } else {
+                            debug!("recv header: {}", header);
+                        }
+                    }
+                };
+                Ok(future::Loop::Continue((reader, buf)))
+            })
+        };
         let skip = response.and_then(|reader| {
             let buf = Vec::with_capacity(64);
-            future::loop_fn((reader, buf), |(reader, mut buf)| {
-                buf.clear();
-                let reader = reader.take(2048);
-                read_until(reader, 0x0a, buf).and_then(|(reader, buf)| {
-                    let reader = reader.into_inner();
-                    if buf.len() <= 2 {
-                        Ok(future::Loop::Break(reader))
-                    } else {
-                        Ok(future::Loop::Continue((reader, buf)))
-                    }
-                })
-            })
+            let reader = reader.take(8 * 1024);
+            future::loop_fn((reader, buf), skip_headers)
+                .map(|reader| reader.into_inner())
         });
-        // FIXME: may lost data in buffer
+        // FIXME: may lost data in buffer?
         Box::new(skip.map(|reader| reader.into_inner()))
     }
 }

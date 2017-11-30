98dc52206382c49fe55745b21ffbd10ebfa8c0a2
sorz <orz@sorz.org>
2017-12-01 01:47:19 +0800

    Refactor: remove duplication on updating delays

--- src/monitor.rs
+++ src/monitor.rs
@@ -2,6 +2,7 @@ extern crate rand;
 use std;
 use std::time::{Instant, Duration};
 use std::io;
+use std::slice::Iter;
 use std::sync::{Mutex, Arc, MutexGuard};
 use self::rand::Rng;
 use ::tokio_timer::Timer;
@@ -42,9 +43,19 @@ impl ServerList {
         self.inner.lock().unwrap()
     }
 
-    fn resort(&self) {
+    /// Update delays and scores, and resort server list.
+    ///
+    /// `delays` must have the same order of inner server list.
+    /// `penalty` would be added to score when the last score is `None`.
+    fn update_delay(&self, delays: Iter<Option<Duration>>, penalty: i32) {
+        let mut infos = self.get();
+        infos.iter_mut().zip(delays).for_each(|(info, delay)| {
+            info.delay = *delay;
+            info.score = delay.map(|t| to_ms(t) + info.server.score_base)
+                .map(|s| (info.score.unwrap_or(s + penalty) * 9 + s) / 10);
+        });
         let mut rng = rand::thread_rng();
-        self.get().sort_by_key(move |info| {
+        infos.sort_by_key(move |info| {
             info.score.unwrap_or(std::i32::MAX - 50) +
             (rng.next_u32() % 30) as i32
         });
@@ -54,12 +65,8 @@ impl ServerList {
 pub fn monitoring_servers(servers: Arc<ServerList>, probe: u64, handle: Handle)
         -> Box<Future<Item=(), Error=()>> {
     let init = test_all(&servers, &handle).and_then(move |delays| {
-        servers.get().iter_mut().zip(delays.iter()).for_each(|(info, t)| {
-            info.delay = *t;
-            info.score = t.map(|t| to_ms(t) + info.server.score_base);
-        });
-        servers.resort();
-        info!("scores:{}", info_stats(servers.get().as_slice()));
+        servers.update_delay(delays.iter(), 0);
+        info!("scores:{}", info_stats(&servers));
         Ok(servers)
     });
     let timer = Timer::default();
@@ -73,14 +80,8 @@ pub fn monitoring_servers(servers: Arc<ServerList>, probe: u64, handle: Handle)
                 test_all(&servers, &handle)
                     .map(|ts| (ts, servers, handle))
             }).and_then(|(ts, servers, handle)| {
-                servers.get().iter_mut().zip(ts.iter()).for_each(|(info, t)| {
-                    info.delay = *t;
-                    info.score = t.map(|t| to_ms(t) + info.server.score_base)
-                        .map(|t|
-                            (info.score.unwrap_or(t + 2000) * 9 + t) / 10);
-                });
-                servers.resort();
-                info!("scores:{}", info_stats(servers.get().as_slice()));
+                servers.update_delay(ts.iter(), 2000);
+                info!("scores:{}", info_stats(&servers));
                 Ok((servers, handle))
             }).and_then(|args| Ok(future::Loop::Continue(args)))
         })
@@ -88,9 +89,9 @@ pub fn monitoring_servers(servers: Arc<ServerList>, probe: u64, handle: Handle)
     Box::new(update)
 }
 
-fn info_stats(infos: &[ServerInfo]) -> String {
+fn info_stats(servers: &ServerList) -> String {
     let mut stats = String::new();
-    for info in infos.iter().take(5) {
+    for info in servers.get().iter().take(5) {
         stats += &match info.score {
             None => format!(" {}: --,", info.server.tag),
             Some(t) => format!(" {}: {},", info.server.tag, t),

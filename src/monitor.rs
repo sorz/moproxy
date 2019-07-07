sorz/moproxy

commit 8e06131238c6f87c3efea267c7b6df3adc85fbbb

Fix lost priorities temporarily after reload

src/monitor.rs
@@ -63,6 +63,7 @@ impl Monitor {
         for server in servers.iter() {
             meters.insert(server.clone(), Meter::new());
         }
+        self.resort();
     }
 
     fn resort(&self) {

commit a97ff1a0d20cc4ba7fc7a2b3b0f1c6903d4c33da

Fix dead lock on reload
Fix the bug introduced by 8e06131238c6f87c3efea267c7b6df3adc85fbbb.

src/monitor.rs
@@ -63,6 +63,7 @@ impl Monitor {
         for server in servers.iter() {
             meters.insert(server.clone(), Meter::new());
         }
+        drop(servers);
         self.resort();
     }
 
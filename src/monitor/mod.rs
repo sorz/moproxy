sorz/moproxy

commit ea5a5591d1845692408f97379cbd5881ea7f4f8c

Reduce lock() call

src/monitor/mod.rs
@@ -68,7 +68,8 @@ impl Monitor {
 
     fn resort(&self) {
         let mut rng = rand::thread_rng();
-        self.servers.lock().sort_by_key(move |server| {
+        let mut servers = self.servers.lock();
+        servers.sort_by_key(move |server| {
             server.score().unwrap_or(std::i32::MAX) - (rng.gen::<u8>() % 30) as i32
         });
         debug!("scores:{}", info_stats(&*self.servers.lock()));
@@ -155,15 +156,15 @@ async fn send_metrics(monitor: &Monitor, graphite: &mut Graphite) -> io::Result<
         .flat_map(|server| {
             let now = Some(SystemTime::now());
             let r = |path, value| Record::new(server.graphite_path(path), value, now);
-            let traffic = server.traffic();
+            let status = server.status_snapshot();
             vec![
-                server.delay().map(|t| r("delay", t.millis() as u64)),
-                server.score().map(|s| r("score", s as u64)),
-                Some(r("tx_bytes", traffic.tx_bytes as u64)),
-                Some(r("rx_bytes", traffic.rx_bytes as u64)),
-                Some(r("conns.total", server.conn_total() as u64)),
-                Some(r("conns.alive", server.conn_alive() as u64)),
-                Some(r("conns.error", server.conn_error() as u64)),
+                status.delay.map(|t| r("delay", t.millis() as u64)),
+                status.score.map(|s| r("score", s as u64)),
+                Some(r("tx_bytes", status.traffic.tx_bytes as u64)),
+                Some(r("rx_bytes", status.traffic.rx_bytes as u64)),
+                Some(r("conns.total", status.conn_total as u64)),
+                Some(r("conns.alive", status.conn_alive as u64)),
+                Some(r("conns.error", status.conn_error as u64)),
             ]
         })
         .filter_map(|v| v)

src/proxy/mod.rs
@@ -2,6 +2,7 @@ pub mod copy;
 pub mod http;
 pub mod socks5;
 use log::debug;
+use parking_lot::Mutex;
 use serde_derive::Serialize;
 use std::{
     fmt,
@@ -10,7 +11,6 @@ use std::{
     net::{IpAddr, SocketAddr},
     ops::{Add, AddAssign},
     str::FromStr,
-    sync::{Mutex, MutexGuard},
     time::Duration,
 };
 use tokio::net::TcpStream;
@@ -57,15 +57,15 @@ pub struct ProxyServer {
 }
 
 #[derive(Debug, Serialize, Clone, Copy, Default)]
-struct ProxyServerStatus {
+pub struct ProxyServerStatus {
     #[serde(skip_serializing)]
     probe_at_least_once: bool,
-    delay: Option<Duration>,
-    score: Option<i32>,
-    traffic: Traffic,
-    conn_alive: u32,
-    conn_total: u32,
-    conn_error: u32,
+    pub delay: Option<Duration>,
+    pub score: Option<i32>,
+    pub traffic: Traffic,
+    pub conn_alive: u32,
+    pub conn_total: u32,
+    pub conn_error: u32,
 }
 
 impl Hash for ProxyServer {
@@ -204,7 +204,7 @@ impl ProxyServer {
     }
 
     pub fn replace_status(&self, from: &Self) {
-        *self.status() = *from.status();
+        *self.status.lock() = *from.status.lock();
     }
 
     pub async fn connect<T>(&self, addr: &Destination, data: Option<T>) -> io::Result<TcpStream>
@@ -228,32 +228,20 @@ impl ProxyServer {
         Ok(stream)
     }
 
-    fn status(&self) -> MutexGuard<ProxyServerStatus> {
-        self.status.lock().unwrap()
-    }
-
-    pub fn delay(&self) -> Option<Duration> {
-        self.status().delay
+    pub fn status_snapshot(&self) -> ProxyServerStatus {
+        *self.status.lock()
     }
 
     pub fn score(&self) -> Option<i32> {
-        self.status().score
+        self.status.lock().score
     }
 
-    pub fn conn_alive(&self) -> u32 {
-        self.status().conn_alive
-    }
-
-    pub fn conn_total(&self) -> u32 {
-        self.status().conn_total
-    }
-
-    pub fn conn_error(&self) -> u32 {
-        self.status().conn_error
+    pub fn traffic(&self) -> Traffic {
+        self.status.lock().traffic
     }
 
     pub fn update_delay(&self, delay: Option<Duration>) {
-        let mut status = self.status();
+        let mut status = self.status.lock();
         status.delay = delay;
         status.score = if !status.probe_at_least_once {
             // First update, just set it
@@ -279,23 +267,21 @@ impl ProxyServer {
     }
 
     pub fn add_traffic(&self, traffic: Traffic) {
-        self.status().traffic += traffic;
+        self.status.lock().traffic += traffic;
     }
 
     pub fn update_stats_conn_open(&self) {
-        self.status().conn_alive += 1;
-        self.status().conn_total += 1;
+        let mut status = self.status.lock();
+        status.conn_alive += 1;
+        status.conn_total += 1;
     }
 
     pub fn update_stats_conn_close(&self, has_error: bool) {
-        self.status().conn_alive -= 1;
+        let mut status = self.status.lock();
+        status.conn_alive -= 1;
         if has_error {
-            self.status().conn_error += 1;
+            status.conn_error += 1;
         }
-    }
-
-    pub fn traffic(&self) -> Traffic {
-        self.status().traffic
     }
 
     pub fn graphite_path(&self, suffix: &str) -> String {

src/web/mod.rs
@@ -103,27 +103,28 @@ fn plaintext_status(start_time: &Instant, monitor: &Monitor) -> http::Result<Res
     table.set_format(*FORMAT_NO_LINESEP_WITH_TITLE);
 
     for ServerStatus { server, throughput } in status.servers {
+        let status = server.status_snapshot();
         let row = table.add_empty_row();
         // Server
         row.add_cell(cell!(l -> server.tag));
         // Score
-        if let Some(v) = server.score() {
+        if let Some(v) = status.score {
             row.add_cell(cell!(r -> v));
         } else {
             row.add_cell(cell!(r -> "-"));
         }
         // Delay
-        if let Some(v) = server.delay() {
+        if let Some(v) = status.delay {
             row.add_cell(cell!(r -> v.format_millis()));
         } else {
             row.add_cell(cell!(r -> "-"));
         }
         // CUR TTL
-        row.add_cell(cell!(r -> server.conn_alive()));
-        row.add_cell(cell!(r -> server.conn_total()));
+        row.add_cell(cell!(r -> status.conn_alive));
+        row.add_cell(cell!(r -> status.conn_total));
         // Up Down
-        row.add_cell(cell!(r -> to_human_bytes(server.traffic().tx_bytes)));
-        row.add_cell(cell!(r -> to_human_bytes(server.traffic().rx_bytes)));
+        row.add_cell(cell!(r -> to_human_bytes(status.traffic.tx_bytes)));
+        row.add_cell(cell!(r -> to_human_bytes(status.traffic.rx_bytes)));
         // ↑↓
         if let Some(tp) = throughput {
             let sum = tp.tx_bps + tp.rx_bps;

Cargo.toml
@@ -19,9 +19,9 @@ serde_json = "1.0"
 serde_derive = "1.0"
 rust-ini = "0.13"
 hyper = {version = "0.13.0-alpha.1", optional = true, default-features = false, features = ["unstable-stream"]}
-parking_lot = {version = "0.8", features = ["serde"]}
+parking_lot = {version = "0.9", features = ["serde"]}
 http = "0.1"
-prettytable-rs = "0.8"
+prettytable-rs = {version = "0.8", default-features = false}
 regex = "1"
 lazy_static = "1.3"
 number_prefix = "0.3"

src/main.rs
@@ -1,4 +1,4 @@
-use clap::{AppSettings, load_yaml};
+use clap::{load_yaml, AppSettings};
 use futures::stream::StreamExt;
 use ini::Ini;
 use log::{debug, error, info, LevelFilter};

commit 897af5ef04e9c7bcdbbad2df68c715b8c7627ac8

Fix deadlock when debug on
Bug introduced by ea5a5591d1845692408f97379cbd5881ea7f4f8c

src/monitor/mod.rs
@@ -84,7 +84,7 @@ impl Monitor {
         servers.sort_by_key(move |server| {
             server.score().unwrap_or(std::i32::MAX) - (rng.gen::<u8>() % 30) as i32
         });
-        debug!("scores:{}", info_stats(&*self.servers.lock()));
+        debug!("scores:{}", info_stats(&servers));
     }
 
     /// Start monitoring delays.
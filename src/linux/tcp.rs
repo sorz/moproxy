dc007b88c0b1aba9a449030989a9583bc62101e5
Shell Chen <me@sorz.org>
2021-10-24 01:07:05 +0800

    Update nix & env_logger

--- src/linux/tcp.rs
+++ src/linux/tcp.rs
@@ -5,8 +5,7 @@ use nix::{
 };
 use std::{
     ffi::OsStr,
-    io::{self, ErrorKind},
-    mem,
+    io, mem,
     net::{SocketAddrV4, SocketAddrV6},
     os::unix::io::AsRawFd,
 };
@@ -15,15 +14,11 @@ pub fn get_original_dest<F>(fd: &F) -> io::Result<SocketAddrV4>
 where
     F: AsRawFd,
 {
-    let addr = getsockopt(fd.as_raw_fd(), OriginalDst).map_err(|e| match e {
-        nix::Error::Sys(err) => io::Error::from(err),
-        _ => io::Error::new(ErrorKind::Other, e),
-    })?;
-    let addr = SocketAddrV4::new(
+    let addr = getsockopt(fd.as_raw_fd(), OriginalDst)?;
+    Ok(SocketAddrV4::new(
         u32::from_be(addr.sin_addr.s_addr).into(),
         u16::from_be(addr.sin_port),
-    );
-    Ok(addr)
+    ))
 }
 
 pub fn get_original_dest6<F>(fd: &F) -> io::Result<SocketAddrV6>
@@ -42,7 +37,7 @@ where
         )
     };
     if res != 0 {
-        return Err(io::Error::new(ErrorKind::Other, "getsockopt fail"));
+        return Err(io::Error::last_os_error());
     }
     let addr = SocketAddrV6::new(
         sockaddr.sin6_addr.s6_addr.into(),

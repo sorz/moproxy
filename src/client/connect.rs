aa5197fd35bfd868d82426aa63d58c25c672bb9c
Shell Chen <me@sorz.org>
2023-03-17 17:18:10 +0800

    Refactor upstream connect

--- src/client/connect.rs
+++ src/client/connect.rs
@@ -1,6 +1,5 @@
 use bytes::Bytes;
 use std::{
-    cmp,
     collections::VecDeque,
     future::Future,
     io::{self, ErrorKind},
@@ -13,20 +12,26 @@ use tracing::{info, instrument};
 
 use crate::proxy::{Destination, ProxyServer};
 
-#[instrument(skip_all, fields(proxy = %server.tag))]
-async fn try_connect(
+#[derive(Debug, Clone)]
+struct Request {
     dest: Destination,
-    server: Arc<ProxyServer>,
     pending_data: Option<Bytes>,
     wait_response: bool,
-) -> io::Result<TcpStream> {
+}
+
+#[instrument(skip_all, fields(proxy = %server.tag))]
+async fn try_connect(request: Request, server: Arc<ProxyServer>) -> io::Result<TcpStream> {
     let max_wait = server.max_wait();
     // waiting for proxy server connected
-    let stream = timeout(max_wait, server.connect(&dest, pending_data)).await??;
+    let stream = timeout(
+        max_wait,
+        server.connect(&request.dest, request.pending_data),
+    )
+    .await??;
 
     // waiting for response data
-    if wait_response {
-        let mut buf = [0u8; 8];
+    if request.wait_response {
+        let mut buf = [0u8; 4];
         let len = timeout(max_wait, stream.peek(&mut buf)).await??;
         if len == 0 {
             return Err(io::Error::new(ErrorKind::UnexpectedEof, "no response data"));
@@ -42,13 +47,12 @@ type PinnedConnectFuture = Pin<Box<dyn Future<Output = io::Result<TcpStream>> + 
 /// connect. Once any of them connected, move that to `reading` and wait
 /// for read respone. Once any of handshakings done, return it and cancel
 /// others.
-pub struct TryConnectAll<'a> {
-    dest: &'a Destination,
-    pending_data: Option<Bytes>,
+pub struct TryConnectAll {
+    request: Request,
     parallel_n: usize,
-    wait_response: bool,
     standby: VecDeque<Arc<ProxyServer>>,
     connects: VecDeque<(Arc<ProxyServer>, PinnedConnectFuture)>,
+    last_error: Option<io::Error>,
 }
 
 pub fn try_connect_all(
@@ -58,33 +62,32 @@ pub fn try_connect_all(
     wait_response: bool,
     pending_data: Option<Bytes>,
 ) -> TryConnectAll {
-    let parallel_n = cmp::max(1, parallel_n);
+    let parallel_n = parallel_n.clamp(1, if wait_response { servers.len() } else { 1 });
     let servers = servers.into_iter().collect();
-    TryConnectAll {
-        dest,
-        parallel_n,
+    let request = Request {
+        dest: dest.clone(),
         pending_data,
         wait_response,
+    };
+    TryConnectAll {
+        request,
+        parallel_n,
         standby: servers,
         connects: VecDeque::with_capacity(parallel_n),
+        last_error: None,
     }
 }
 
-impl<'a> Future for TryConnectAll<'a> {
-    type Output = Option<(Arc<ProxyServer>, TcpStream)>;
+impl Future for TryConnectAll {
+    type Output = io::Result<(Arc<ProxyServer>, TcpStream)>;
 
-    fn poll(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context,
-    ) -> Poll<Option<(Arc<ProxyServer>, TcpStream)>> {
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
         loop {
-            let dest = self.dest.clone();
             // if current connections less than parallel_n,
             // pick servers from queue to connect.
             while !self.standby.is_empty() && self.connects.len() < self.parallel_n {
                 let server = self.standby.pop_front().unwrap();
-                let data = self.pending_data.clone();
-                let conn = try_connect(dest.clone(), server.clone(), data, self.wait_response);
+                let conn = try_connect(self.request.clone(), server.clone());
                 self.connects.push_back((server, Box::pin(conn)));
             }
 
@@ -95,19 +98,23 @@ impl<'a> Future for TryConnectAll<'a> {
                 match conn.as_mut().poll(cx) {
                     // error, stop trying, drop it.
                     Poll::Ready(Err(err)) => {
-                        info!(proxy = %server.tag, ?err, "Failed");
+                        info!(proxy = %server.tag, ?err, "Failed to connect upstream proxy");
+                        self.last_error = Some(err);
                         drop(self.connects.remove(i));
                     }
                     // not ready, keep here, poll next one.
                     Poll::Pending => i += 1,
                     // ready, return it.
-                    Poll::Ready(Ok(conn)) => return Poll::Ready(Some((server.clone(), conn))),
+                    Poll::Ready(Ok(conn)) => return Poll::Ready(Ok((server.clone(), conn))),
                 }
             }
 
             // if all servers failed, return error
             if self.connects.is_empty() && self.standby.is_empty() {
-                return Poll::Ready(None);
+                let err = self.last_error.take().unwrap_or_else(|| {
+                    io::Error::new(io::ErrorKind::InvalidInput, "no upstream proxy")
+                });
+                return Poll::Ready(Err(err));
             }
 
             // if not need to connect standby server, wait for events.

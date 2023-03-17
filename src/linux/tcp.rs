b5571bb02b05c704e993b750d3ba127ea657c569
Shell Chen <me@sorz.org>
2023-03-17 15:53:37 +0800

    Replace libc::setsockopt with nix

--- src/linux/tcp.rs
+++ src/linux/tcp.rs
@@ -1,12 +1,10 @@
-use libc::{self, c_void, setsockopt, socklen_t, IPPROTO_TCP, TCP_CONGESTION};
-use nix::{
-    self,
-    sys::socket::{getsockopt, sockopt::OriginalDst},
+use nix::sys::socket::{
+    getsockopt, setsockopt,
+    sockopt::{Ip6tOriginalDst, OriginalDst, TcpCongestion},
 };
 use std::{
     ffi::OsStr,
     io::{self, ErrorKind},
-    mem,
     net::{SocketAddr, SocketAddrV4, SocketAddrV6},
     os::unix::io::AsRawFd,
 };
@@ -36,21 +34,9 @@ impl TcpStreamExt for TcpStream {
 
 impl TcpListenerExt for TcpListener {
     fn set_congestion<S: AsRef<OsStr>>(&self, alg: S) -> io::Result<()> {
-        let alg = alg.as_ref();
-        let ret = unsafe {
-            setsockopt(
-                self.as_raw_fd(),
-                IPPROTO_TCP,
-                TCP_CONGESTION,
-                alg as *const _ as *const c_void,
-                mem::size_of_val(alg) as socklen_t,
-            )
-        };
-        if ret == 0 {
-            Ok(())
-        } else {
-            Err(io::Error::last_os_error())
-        }
+        let val = alg.as_ref().into();
+        setsockopt(self.as_raw_fd(), TcpCongestion, &val)?;
+        Ok(())
     }
 }
 
@@ -69,25 +55,11 @@ fn get_original_dest_v6<F>(fd: &F) -> io::Result<SocketAddrV6>
 where
     F: AsRawFd,
 {
-    let mut sockaddr: libc::sockaddr_in6 = unsafe { mem::zeroed() };
-    let mut socklen = mem::size_of::<libc::sockaddr_in6>();
-    let res = unsafe {
-        libc::getsockopt(
-            fd.as_raw_fd(),
-            libc::SOL_IPV6,
-            libc::SO_ORIGINAL_DST,
-            &mut sockaddr as *mut _ as *mut c_void,
-            &mut socklen as *mut _ as *mut socklen_t,
-        )
-    };
-    if res != 0 {
-        return Err(io::Error::last_os_error());
-    }
-    let addr = SocketAddrV6::new(
+    let sockaddr = getsockopt(fd.as_raw_fd(), Ip6tOriginalDst)?;
+    Ok(SocketAddrV6::new(
         sockaddr.sin6_addr.s6_addr.into(),
         u16::from_be(sockaddr.sin6_port),
         sockaddr.sin6_flowinfo,
         sockaddr.sin6_scope_id,
-    );
-    Ok(addr)
+    ))
 }
